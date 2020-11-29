53469185302799e34c5782932d9432008d541203
kaneyan0607 <kaneyan0607@gmail.com>
2020-11-30 04:01:20 +0900

    add:memoアプリの作成。接続プログラムを共通プログラムにした。

---
+++ memo3/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>PHP</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="font-weight-normal">PHP</h1>
+    </header>
+
+    <main>
+        <h2>Practice</h2>
+        <!-- ここにプログラムを記述します -->
+        <?php
+
+        require('dbconnect.php');
+
+        $memos = $db->query('SELECT * FROM memos ORDER BY id DESC');
+        ?>
+        <article>
+            <?php while ($memo = $memos->fetch()) : ?>
+                <p><a href="memo.php?id=<?php print($memo['id']); ?>"><?php print(mb_substr($memo['memo'], 0, 50)); ?></a></p>
+                <time><?php print($memo['created_at']); ?></time>
+                <hr>
+            <?php endwhile; ?>
+        </article>
+    </main>
+</body>
+
+</html>
